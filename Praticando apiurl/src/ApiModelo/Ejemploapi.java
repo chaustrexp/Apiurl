019a91ed7b380e60b392d7dc0dfcb13421478f42
cristiqn <cristianchaustre90@gmail.com>
2026-08-19 09:18:47 -0500

    practicando sobre api

---
+++ Praticando apiurl/src/ApiModelo/Ejemploapi.java
@@ -0,0 +1,15 @@
+
+package ApiModelo;
+
+public class Ejemploapi {
+
+    public static void main(String[] args) {
+        String dato_url = "https://jsonplaceholder.typicode.com/posts";
+        ApiModelo obj_api = new ApiModelo(dato_url);
+        obj_api.hacer_peticion_get();
+        //obj_api.hacer_peticion_post();
+        obj_api.imprimir_info();
+        obj_api.info_respuesta();
+    }
+}
+
